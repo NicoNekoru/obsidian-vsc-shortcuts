c3dece8da3bd662b6eaee427affd564792445820
Neko <66227158+NicoNekoru@users.noreply.github.com>
2023-08-14 13:06:37 -0700

    boilerplate

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,23 @@ export class VSCShortcuts extends Plugin
 	async onload() 
 	{
 		console.log('loading vsc shortcuts plugin');
+
+		this.addCommand({
+			id: '',
+			name: '',
+			editorCheckCallback(checking, editor, ctx) 
+			{
+				checking;
+				editor;
+				ctx;
+			},
+		});
 	}
 
 	onunload() 
 	{
 		console.log('unloading vsc shortcuts plugin');
 	}
-
 }
 
 export default VSCShortcuts;
